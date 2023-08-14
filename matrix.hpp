390531399ec0baa3d8f1e77e4e6ced6fb3b8c0ea
Francisco del Campo <frankiedelcampo@rocketmail.com>
2023-08-13 22:54:51 -0400

    Added functionality to matrix, created more header files, added important comments

--- matrix.hpp
+++ matrix.hpp
@@ -2,14 +2,21 @@
 
 #include <vector>
 
+// Eventually, this should deal with generic numeric types (i.e, ints, doubles, floats, etc)
+
 class Matrix{
 public:
-    Matrix(size_t numRows, size_t numCols);
+    Matrix(size_t num_rows, size_t num_cols, std::vector<std::vector<double>> data);
+    Matrix(size_t num_rows, size_t num_cols);
     Matrix();
+    Matrix(double vec_data[], size_t len_vec, bool row_vec);
+    Matrix transpose() const;
+    static Matrix identity(size_t rows, size_t cols);
     Matrix operator+(const Matrix& other) const;
-    Matrix hadamard(const Matrix& other) const;
+    Matrix operator-(const Matrix& other) const;
     Matrix operator*(const Matrix& other) const;
     Matrix operator*(const float scalar) const;
+    Matrix hadamard(const Matrix& other) const;
     // ~Matrix();
     // testing git
     // Instance Fields
--- matrix_test.cpp
+++ matrix_test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include "Matrix.hpp"
+#include "matrix.hpp"
 
 int main() {
     // size_t rows = 2;
@@ -9,8 +9,76 @@ int main() {
     // std::vector<std::vector<double>> data;
     Matrix test = Matrix();
     printf("asdfhk");
-    printf("%d", test.data[0][0]);
+    printf("\n");
+    std::cout << test.data[0][0];
 
+    Matrix test2 = Matrix::identity(3, 3);
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++) {
+            std::cout << test2.data[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    Matrix test3 = Matrix(3, 3);
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++) {
+            std::cout << test3.data[i][j];
+        }
+        std::cout << std::endl;
+    }
+    
+    float scalar = 2.0;
+    Matrix test4 = test2*scalar;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++) {
+            std::cout << test4.data[i][j];
+        }
+        std::cout << std::endl;
+    }
+    Matrix test5 = test4-test2;
+
+    Matrix test6 = test2*test4;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++) {
+            std::cout << test6.data[i][j];
+        }
+        std::cout << std::endl;
+    }
+    
+    double arr1[2] = {1, 2};
+    double arr2[2] = {3, 4};
+
+    std::vector<std::vector<double>> two_d_vector;
+
+    for (int i = 0; i < 2; ++i) {
+        std::vector<double> row;
+        row.push_back(arr1[i]);
+        row.push_back(arr2[i]);
+        two_d_vector.push_back(row);
+    }
+
+    
+    printf("\n");
+    Matrix test7 = Matrix(2, 2, two_d_vector);
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++) {
+            std::cout << test7.data[i][j];
+        }
+        std::cout << std::endl;
+    }
+    printf("\n");
+
+    
+    
+    Matrix test8 = test7.transpose();
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++) {
+            std::cout << test8.data[i][j];
+        }
+        std::cout << std::endl;
+    }
+    
     return 0;
 
 
